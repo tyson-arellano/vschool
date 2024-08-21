0622bbd0c8e61db22f2783b96022862063253ec0
Tyson Arellano <tyson.arellano@yahoo.com>
2024-08-21 09:53:09 -0600

    solved

--- curriculum/level-1/601-daily-planet-editor/paper.js
+++ curriculum/level-1/601-daily-planet-editor/paper.js
@@ -9,41 +9,41 @@ just fix the existing ones.
 
 // Written by Kent, Clark
 
-var Enemies=["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
+var enemyName = ["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
 
-funtion Who_Wins(is-there-kryptonite,enemy_name)
+function Who_Wins(isThereKryptonite,enemyName)
 {
-if (!is-there-kryptonite)
+if (isThereKryptonite)
 {
-    return "Superman beats " +enemyName+ ", of course";
+    return "Superman beats " + enemyName + " , of course";
 }
 
 else{
-    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyname +" could possibly win this one.";
+    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyName +" could possibly win this one.";
 }
     }
 
-for (var i=0;i<enemies.length;i++)
+for (var i=0;i<enemyName.length;i++)
 {
-    var is_there_kryptonite:
-    if (i%2=== 0) {
-        is_there_kryptonite = true;
+    var isThereKryptonite;
+    if (i%2 === 0) {
+        isThereKryptonite = true;
     } else {
-        is_there_kryptonite = false;
+        isThereKryptonite = false;
     }
-    console.log(Who_Wins(is_there_kryptonite,enemies[i]));
+    console.log(Who_Wins(isThereKryptonite,enemyName[i]));
 }
 
 function HowAttractedIsLoisLaneToMe ()
 {
     // 1 is not at all attracted, 10 is "super" attracted...
-    return Math.floor((Mat.random()*10)+1);
+    return Math.floor((Math.random()*10)+1);
 }
 
-consol.log( HowAttractedIsLoisLaneToMe ()) ;
+console.log( HowAttractedIsLoisLaneToMe ()) ;
 
-var clarkKent =true;
-var superman false;
+var clarkKent = true;
+var superman = false;
 
 while (clarkKent){
     console.log("I'm just a nerdy columnist");
