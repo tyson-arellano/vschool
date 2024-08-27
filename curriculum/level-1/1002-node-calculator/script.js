fa1dbdb44795fcf6ad77e11085b929ca29c47aa5
Tyson Arellano <tyson.arellano@yahoo.com>
2024-08-27 17:55:52 -0600

    8/27

--- curriculum/level-1/1002-node-calculator/script.js
+++ curriculum/level-1/1002-node-calculator/script.js
@@ -1,23 +1,23 @@
 const readlineSync = require("readline-sync")
 
-var num1 = readlineSync.question('Please enter your first number ')
+var num1 = parseInt(readlineSync.question('Please enter your first number '))
 
-var num2 = readlineSync.question('Please enter your second number ')
+var num2 = parseInt(readlineSync.question('Please enter your second number '))
 
 var operation = readlineSync.question('Please enter the operation you want to perform: add, sub, mul, div: ')
 
 function performOperation() {
 var result 
 
-    if (operation === 'add') {
-    result = num1 + num2
-} else if (operation === 'sub') {
+   if (operation === 'sub') {
     result = num1 - num2
 } else if (operation === 'mul') {
     result = num1 * num2
-} else if (operation === 'div') 
+} else if (operation === 'div'){ 
     result = num1 / num2
-
+} else if (operation === 'add') {
+    result = num1 + num2
+    }
 console.log("The result is: " + result + "") 
 
 }
