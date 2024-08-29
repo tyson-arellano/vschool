ba99887be1c125ab2d94df2eec16e0da6b6a0ed3
Tyson Arellano <tyson.arellano@yahoo.com>
2024-08-29 06:12:22 -0600

    8/29

--- curriculum/level-1/1105-loop-olympics/script.js
+++ curriculum/level-1/1105-loop-olympics/script.js
@@ -1,6 +1,35 @@
-for (var i = 0; i < 10; i++) {
-    console.log(i);
-}
-for (var i = 9; i >= 0; i--) {
-    console.log(i);
-}
+//Preliminaries
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// for (var i = 9; i >= 0; i--) {
+//     console.log(i);
+// }
+
+//BRONZE
+
+// const numbers = [];
+// for (var i = 0; i < 10; i++) {
+//   numbers.push(i);
+// }
+// console.log(numbers) 
+
+// for (var i = 0; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//       console.log(i);
+//     }
+//   }
+
+// for (var i = 0; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//       console.log(i)
+//     }
+//   }
+  const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"];
+  const everyOtherFruit = [];
+  
+  for (var i = 0; i < fruit.length; i += 2) {
+    everyOtherFruit.push(fruit[i]);
+  }
+  
+  console.log(everyOtherFruit); 
