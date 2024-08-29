afa0bc49f0d17b9236558f1c94ff6899adb8b73a
Tyson Arellano <tyson.arellano@yahoo.com>
2024-08-29 15:27:03 -0600

    8/29

---
+++ curriculum/level-1/1109-forception/script.js
@@ -0,0 +1,16 @@
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function forception(people, alphabet){
+    var result = []
+    for(var i = 0; i < people.length; i++){
+    result.push("" + people[i]) + ":";
+        for( var j = 0; j < alphabet.length; j++){
+            result.push(alphabet[j].toUpperCase());
+        }
+
+    }
+  console.log(result)
+}
+forception(people, alphabet)
+
