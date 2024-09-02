5d6d5230ee673de39ae7227a2785e6c7893bfe71
Tyson Arellano <tyson.arellano@yahoo.com>
2024-09-02 16:52:33 -0600

    8/29

---
+++ curriculum/level-1/1110-string-methods-exercise/script.js
@@ -0,0 +1,34 @@
+
+// 
+    // function capilizeAndLowercase(){
+    //     var capLow = "Hello";
+    //     var upCase = capLow.toUpperCase();
+    //     var lowCase = capLow.toLowerCase();
+    //     return upCase + lowCase;
+    // }
+
+    // console.log(capilizeAndLowercase());
+
+
+// function findMiddleIndex() {
+//     var str  = "Hello World";
+//     var length = Math.floor(str.length / 2);
+//     return length;
+// }
+// console.log(findMiddleIndex());
+
+// function firstHalf(){
+//     var text = "Hello World";
+//     var firstH = text.slice(0,5);
+//     return firstH;
+// }
+// console.log(firstHalf());
+
+// function capilizeAndLowercase(){
+//     var string = "Hello World";
+//     var low = string.slice(0,5).toLowerCase();
+//     var high= string.slice(6).toUpperCase();
+
+//     return low + " " + high;
+// }
+// console.log(capilizeAndLowercase())
