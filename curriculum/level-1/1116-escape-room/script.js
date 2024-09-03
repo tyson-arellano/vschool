c9d2cb345e767ce67b882896a6208cc6d6891983
Tyson Arellano <tyson.arellano@yahoo.com>
2024-09-03 10:57:21 -0600

    9/3

--- curriculum/level-1/1116-escape-room/script.js
+++ curriculum/level-1/1116-escape-room/script.js
@@ -1,7 +1,7 @@
 const readline = require("readline-sync");
 
-let hasKey = false;
-let isGameOver = false;
+var hasKey = false;
+var isGameOver = false;
 while (!isGameOver) {
 
     const choice = readline.question("You find yourself alone in an empty room with a door and a hole in the wall.\nWhat would you like to do?\n(a) Find the key\n(b) Open the door\n(c) Put your hand in the hole\n(Type 'a', 'b', or 'c'): ").toLowerCase();
@@ -9,22 +9,22 @@ while (!isGameOver) {
     switch (choice) {
         case "a":
             if (hasKey) {
-                console.log("You already have the key. Try opening the door.\n");
+                console.log("You already have the key.\n");
             } else {
-                console.log("You found the key! You can now try to open the door.\n");
+                console.log("You found the key!\n");
                 hasKey = true;
             }
             break;
         case "b":
             if (hasKey) {
-                console.log("Congratulations! You used the key to open the door and escaped the room!\n");
+                console.log("You used the key to open the door and escaped the room! You win!\n");
                 isGameOver = true;
             } else {
                 console.log("The door is locked. You need to find the key first.\n");
             }
             break;
         case "c":
-            console.log("Oh no! You put your hand in the hole and something bit you. You died!\n");
+            console.log("You put your hand in the hole and you suddenly vanished into the void. You died!\n");
             isGameOver = true;
             break;
         default:
@@ -32,4 +32,4 @@ while (!isGameOver) {
     }
 }
 
-console.log("Game Over. Thank you for playing!");
+console.log("Game Over. Thanks for playing!");
